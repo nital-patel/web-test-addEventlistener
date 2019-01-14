574415d28dc9db4370257e8e6f670a39341f88be
Nital Patel <patelnital36@gmail.com>
2019-01-14 12:33:30 -0500

    Add script

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+let outer = document.getElementsByClassName('outer')[0];
+let middle = document.getElementsByClassName('middle')[0];
+let inner1 = document.getElementsByClassName('inner1')[0];
+let inner2 = document.getElementsByClassName('inner2')[0];
